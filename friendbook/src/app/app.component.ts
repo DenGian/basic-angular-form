f0dc899cb942385dd70acd9edec7f4f93d23539c
DenGian <mondelaers.ian@gmail.com>
2022-08-04 16:34:42 +0200

    add function for submition

--- friendbook/src/app/app.component.ts
+++ friendbook/src/app/app.component.ts
@@ -7,9 +7,13 @@ import { Friend } from "./friend";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  language0:Array<string> = ['JavaScript','Ruby']
-  language1:Array<string> = ['html','css']
-  language2:Array<string> = ['PHP','Java']
-  language3:Array<string> = ['C/C++','ARM', 'Rust']
+  language0: Array<string> = ['JavaScript', 'Ruby']
+  language1: Array<string> = ['html', 'css']
+  language2: Array<string> = ['PHP', 'Java']
+  language3: Array<string> = ['C/C++', 'ARM', 'Rust']
   friendModel = new Friend(null, null, null, null, null);
+  submitForm(){
+    console.log(this.friendModel);
+  }
 }
+
