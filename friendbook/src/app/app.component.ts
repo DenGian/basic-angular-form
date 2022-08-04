b780b54e9ff7ee27e15654e3020a6a0103d31ba0
DenGian <mondelaers.ian@gmail.com>
2022-08-04 14:30:48 +0200

    add files

---
+++ friendbook/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Friend } from "./friend";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  language0:Array<string> = ['JavaScript','Ruby']
+  language1:Array<string> = ['html','css']
+  language2:Array<string> = ['PHP','Java']
+  language3:Array<string> = ['C/C++','ARM', 'Rust']
+  friendModel = new Friend(null, null, null, null, null);
+}
---
+++ friendbook/src/app/friend.ts
@@ -0,0 +1,15 @@
+export class Friend {
+  _firstName: string | null
+  _lastName: string | null
+  _email: string | null
+  _phoneNumber: number | null
+  _favouriteLanguage: string | null
+
+  constructor(firstName: string | null, lastName: string | null, email: string | null, phoneNumber: number | null, favouriteLanguage: string | null) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._email = email;
+    this._phoneNumber = phoneNumber;
+    this._favouriteLanguage = favouriteLanguage;
+  }
+}
