02ae425cbfede9f184cd02b56a674f234bb57dc9
DenGian <mondelaers.ian@gmail.com>
2022-08-05 11:43:51 +0200

    updated url

--- friendbook/src/app/add-friend.service.ts
+++ friendbook/src/app/add-friend.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Friend } from "./friend";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddFriendService {
-
-  constructor() { }
+  _http : HttpClient
+  _url : string = 'http://localhost:9009/'
+  constructor(http : HttpClient) {
+    this._http = http;
+  }
+  addFriend(data : Friend){
+    return this._http.post(this._url, data)
+  }
 }
