e31dc58583ed58ee51d22c77016a74a768100525
DenGian <mondelaers.ian@gmail.com>
2022-08-07 13:12:19 +0200

    fixed function + beter structure

--- friendbook/src/app/app.component.ts
+++ friendbook/src/app/app.component.ts
@@ -1,24 +1,43 @@
-import { Component } from '@angular/core';
-import { Friend } from "./friend";
-import { AddFriendService } from "./add-friend.service";
+import {Component, OnInit} from '@angular/core';
+import {Friend} from "./friend";
+import {AddFriendService} from "./add-friend.service";
+
+// import * as url from "url";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   language0: Array<string> = ['JavaScript', 'Ruby']
   language1: Array<string> = ['html', 'css']
   language2: Array<string> = ['PHP', 'Java']
   language3: Array<string> = ['C/C++', 'ARM', 'Rust']
   friendModel = new Friend(null, null, null, null, null);
-  constructor(private addFriendService : AddFriendService) {
+  allFriends: Promise<any> | string = '';
+
+  constructor(private addFriendService: AddFriendService) {
   }
-  submitForm(){
+
+  ngOnInit(): any {
+    this.getAllFriends('http://localhost:9009/allFriends')
+  }
+
+  submitForm() {
     let observable = this.addFriendService.addFriend(this.friendModel)
     // console.log(this.friendModel);
-    observable.subscribe(data => console.log('good'), error => console.error('bad'))
+    observable.subscribe(data =>
+        this.getAllFriends('http://localhost:9009/allFriends'),
+      error => console.error('you did an oopsie'));
+  }
+
+  public async getAllFriends(_url: string): Promise<any> {
+    await fetch(_url, {headers: {'Content-Type': 'application/json'}})
+      .then(response => response.json())
+      .then(data => {
+        this.allFriends = data;
+        console.log(this.allFriends);
+      })
   }
 }
-
